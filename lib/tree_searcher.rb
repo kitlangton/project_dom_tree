72f7cfcebbf2c9acb87bfd15efad6d1f6ccbf715
Kit Langton <kitlangton@gmail.com>
2016-01-14 20:32:25 -0500

    IT WORKS

--- lib/tree_searcher.rb
+++ lib/tree_searcher.rb
@@ -16,18 +16,15 @@ class TreeSearcher
     end.flatten.compact
   end
 
-
   def search_children(node, attribute, value)
     search_by(attribute, value, node)
   end
 
-  def search_ancestors(node, attribute, value)
-    node.children.map do |child|
-      if child.matches?(attribute, value)
-        [child, search_by(attribute, value, child)]
-      else
-        search_by(attribute, value, child)
-      end
-    end.flatten.compact
+  def search_ancestors(node, attribute, value, array = [])
+    return array unless node.parent
+
+    array << node.parent if node.parent.matches?(attribute, value)
+
+    search_ancestors(node.parent, attribute, value, array)
   end
 end
