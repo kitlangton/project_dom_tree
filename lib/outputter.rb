d6e8d1b7e0db3ad9c2fc9b680ea1c71438dfab13
Kit Langton <kitlangton@gmail.com>
2016-07-15 20:19:34 -0400

    Update outputter.rb

--- lib/outputter.rb
+++ lib/outputter.rb
@@ -10,22 +10,18 @@ class Outputter
     return unless node
 
     node.display_open
-
-    node.children.each do |child|
-      output(child)
-    end
-
+    node.children.each do { |child| output(child) }
     node.display_close
   end
 end
 
-html = File.read(__dir__ + "/../test.html")
-html_string = "<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>"
-html = html.gsub("\n", " ")
-parser = DOMReader.new
-parser.build_tree(html)
+# html = File.read(__dir__ + "/../test.html")
+# html_string = "<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>"
+# html = html.gsub("\n", " ")
+# parser = DOMReader.new
+# parser.build_tree(html)
 
-outputter = Outputter.new(parser)
+# outputter = Outputter.new(parser)
 # Rainbow.enabled = false
 # $stdout.reopen("out.txt", "w")
-outputter.output
+# outputter.output
